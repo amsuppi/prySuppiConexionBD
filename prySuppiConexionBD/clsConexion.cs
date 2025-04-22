bf7820aa860b851c51f20c995f5f58a5d2b6f2e4
amsuppi <maribelsuppi97@gmail.com>
2025-04-22 19:56:10 -0300

    Agregar archivos de proyecto.

---
+++ prySuppiConexionBD/clsConexion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prySuppiConexionBD
+{
+    internal class clsConexion
+    {
+        public void VerificarConexion()
+        {
+            string connectionString = "Server=localhost;Database=Comercio;Trusted_Connection=True;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    MessageBox.Show("✅ Conexión exitosa.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("❌ Error de conexión: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
